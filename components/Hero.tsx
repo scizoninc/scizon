b0cd19455ed6f1a9603807abc15a026f190e13df
Pedro <daniel@Pedro.localdomain>
2025-08-07 22:32:07 -0300

    Alterations on text,just for today

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,7 @@ export default function Hero() {
   return (
     <section className="text-center my-32 mx-4 sm:mx-8 md:mx-16 lg:mx-24">
       <h1 className="font-sans text-3xl tracking-tighter sm:text-5xl md:text-6xl lg:text-7xl dark:text-white mb-6">
-        Scizon AI
+        Visualize Data with Insights and Advanced Analysis 
       </h1>
       <p className="text-xl text-gray-600 dark:text-gray-300 mb-12 font-light">
         Transform data into ideas.
@@ -14,7 +14,7 @@ export default function Hero() {
           href="https://scizonai.vercel.app"
           className="bg-black text-white dark:bg-white dark:text-black px-5 py-2 rounded-md text-base font-semibold hover:bg-gray-800 dark:hover:bg-gray-200 transition duration-300"
         >
-          Scizon AI
+          Advanced Data AI
         </Link>
         <Link
           href="https://scizonadmin.vercel.app"
